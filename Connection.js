59d52d7840a1fdd664672d10a94a3f1f94c37cb0
SILVANO GOMES DO AMARAL <SAMARAL@bbmapfre.com.br>
2020-07-30 16:09:02 -0300

    Aplicando port redis

--- Connection.js
+++ Connection.js
@@ -1,9 +1,10 @@
 const redis = require('redis')
+const REDIS_PORT = process.env.PORT || 6379
 
 module.exports = {
   createConnection: () => {
     return new Promise((resolve, reject) => {
-      const client = redis.createClient()
+      const client = redis.createClient(REDIS_PORT)
 
       client.on('connect', () => {
         resolve(client)
